76677f95df54e34abac735309f5d714c54faa5cf
dmytro-pelovych <falcon.patron@gmail.com>
2023-05-13 10:56:50 +0300

    Change threshold

--- src/modules/parser/bert_qa/constants.py
+++ src/modules/parser/bert_qa/constants.py
@@ -2,4 +2,4 @@ QA_MODEL_NAME = 'robinhad/ukrainian-qa'
 PIPELINE_TASK_TYPE = 'question-answering'
 CACHE_DIR_PATH = '/Users/dmytro-pelovych/University/SE3/cw/project/cache'
 
-ANSWER_SCORE_THRESH = 0.7
+ANSWER_SCORE_THRESH = 0.5
