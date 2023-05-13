14b6ac3498206bd2ea5be7de7f7f7df8e4138fc8
dmytro-pelovych <falcon.patron@gmail.com>
2023-05-13 10:56:01 +0300

    Move bulk processing flow to UDP client

--- src/modules/udpipe_client/udpipe_client.py
+++ src/modules/udpipe_client/udpipe_client.py
@@ -1,11 +1,23 @@
 import os
 
+from src.modules.udpipe_client.typedefs import Sentence
+
+import src.modules.file_manager.file_manager as fm
 import src.modules.udpipe_client.script_runner.script_runner as sr
 
+from src.modules.udpipe_client.helpers import \
+  filter_extra_comments, \
+  sub_split_into_semistruct, \
+  map_semistruct_to_sentences, \
+  define_parser, \
+  is_process_request_succeeded, \
+  is_udp_running, \
+  delayed, \
+  throw, \
+  log
+
 from src.modules.udpipe_client.constants import UDP_MODEL_NAME, START_UDP_SH_PATH, STOP_UDP_SH_PATH
-from src.modules.udpipe_client.helpers import filter_extra_comments, sub_split_into_semistruct, \
-  map_semistruct_to_sentences, define_parser, is_process_request_succeeded, throw, is_udp_running, log, delayed
-from src.modules.udpipe_client.typedefs import Sentence
+from src.constants.paths import TXT_DATA_DIR_PATH, ANALYSIS_DATA_DIR_PATH
 
 
 def make_sentences_from_udp_result(raw_udp_result: str) -> list[Sentence]:
@@ -15,6 +27,26 @@ def make_sentences_from_udp_result(raw_udp_result: str) -> list[Sentence]:
   sentences = map_semistruct_to_sentences(semistruct_records)
 
   return sentences
+
+
+def bulk_process_by_paths() -> None:
+  paths = fm.get_parsed_paths(TXT_DATA_DIR_PATH, '*.txt')
+
+  start()
+  [process_by_path(path.path) for path in paths]
+  stop()
+
+
+def process_by_path(in_file_path: str) -> None:
+  out_file_path = get_dst_file_path(in_file_path)
+
+  process(in_file_path, out_file_path, locally=True)
+
+
+def get_dst_file_path(src_file_path: str) -> str:
+  file_id = fm.get_file_id(src_file_path)
+
+  return f'{ANALYSIS_DATA_DIR_PATH}/{file_id}.json'
 
 
 def start():
